18a94674eff5736b341cff2fb9a221563f359455
Welkus <vdelkus@gmail.com>
2023-10-22 01:31:35 +0300

    Task Done

--- src/main/java/school/mjc/stage0/loops/task4/SkippingThird.java
+++ src/main/java/school/mjc/stage0/loops/task4/SkippingThird.java
@@ -2,6 +2,14 @@ package school.mjc.stage0.loops.task4;
 
 public class SkippingThird {
     public void printUntilButThird(int lastPrinted) {
+        int i = 0;
+        while ( i < lastPrinted){
+            if(i == 3) continue;
+
+            System.out.println(i);
+            i++;
+
+        }
 
     }
 }
